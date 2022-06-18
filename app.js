d767907dab4ba96c509afa878b2dbbf07455d243
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2022-06-18 11:42:00 -0300

    fix app

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5050
 
-app.listen(() => {
-  console.log('listening on ', port)
-})
-
 app.get('/', (req, res) => {
   res.json({
     ok: true
   })
+})
+
+app.listen(port, () => {
+  console.log('listening on ', port)
 })
