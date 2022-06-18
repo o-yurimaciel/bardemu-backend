926e913132a2d8d8718eeb55cff7db78226eb7ab
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2022-06-18 11:49:18 -0300

    products

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5050
 
+const products = [
+  { name: "Hamburguer de Calabresa", price: 25.00 },
+  { name: "Hamburguer de Carne", price: 22.00 }
+]
+
 app.get('/', (req, res) => {
   res.json({
     message: "Olá, BarDeMu!"
   })
+})
+
+app.get('/products', (req, res) => {
+  res.json(
+    products
+  )
 })
 
 app.listen(port, () => {
